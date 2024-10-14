88661cc86910e3809cc4d0459254710530150322
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-14 21:28:15 +0100

    chore: rename dirs

--- scripts/build.ts
+++ scripts/build.ts
@@ -26,3 +26,30 @@ await $`bun run --filter '*' build`;
 
 /* copy redirects */
 await $`cp -r _redirects ${rootDist}/`;
+
+/* generate json */
+{
+	const projects = await glob('*/*.md', {
+		onlyFiles: true,
+	});
+	const projectsData = await Promise.all(
+		projects.map(async (project) => {
+			const projectDirName = p.dirname(project);
+			const content = await $`cat ${project}`;
+			const title = content.match(/title: (.*)/)?.[1];
+			const description = content.match(/description: (.*)/)?.[1];
+			const tags = content.match(/tags: (.*)/)?.[1].split(',').map(tag => tag.trim());
+			const pdf = await glob(`${projectDirName}/*.pdf`, {
+				onlyFiles: true,
+			});
+			return {
+				title,
+				description,
+				tags,
+				pdf: pdf[0] ? p.join(projectDirName, p.basename(pdf[0])) : undefined,
+			};
+		}),
+	);
+	await $`mkdir -p ${rootDist}`;
+	await $`echo '${JSON.stringify(projectsData)}' > ${p.join(rootDist, 'projects.json')}`;
+}
