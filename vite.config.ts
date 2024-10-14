036a9eac971cfd35f8166cfe95c7c20b9fe0ad47
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-14 17:12:59 +0100

    feat: add vite config

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import MarkdownItMagicLink from 'markdown-it-magic-link'
+import { defineConfig } from 'vite'
+import '@slidev/cli'
+
+export default defineConfig({
+  slidev: {
+    markdown: {
+      markdownItSetup(md) {
+        md.use(MarkdownItMagicLink, {
+          linksMap: {
+            'vim-jp': {link:'https://vim-jp.org/', imageUrl:'https://vim-jp.org/assets/images/vim2-128.png'},
+          },
+        })
+      },
+    },
+  },
+})
