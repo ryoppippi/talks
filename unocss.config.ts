5abc919ed476e5ce08a4bf94ae8813063b4c9fd0
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-14 18:59:05 +0100

    feat: unocss config

---
+++ unocss.config.ts
@@ -0,0 +1,13 @@
+import { presetRyoppippi } from '@ryoppippi/unocss-preset';
+import slidevUno from '@slidev/client/uno.config';
+
+import { mergeConfigs } from 'unocss';
+
+export default mergeConfigs([
+	slidevUno,
+	{
+		presets: [
+			presetRyoppippi(),
+		],
+	},
+]);
