991199139a6f7ed2442aef5a958ad1cf41a05fc2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-15 16:18:20 +0100

    chore(markdown): figure

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
+/* eslint-disable ts/no-unsafe-argument */
+
 import MarkdownItMagicLink from 'markdown-it-magic-link';
 import { defineConfig } from 'vite';
+
 import '@slidev/cli';
+// @ts-expect-error no types
+import Figures from 'markdown-it-image-figures';
 
 export default defineConfig({
 	slidev: {
 		markdown: {
 			markdownItSetup(md) {
+				md.use(Figures, {
+					figcaption: true,
+					lazy: true,
+					async: true,
+				});
 				md.use(MarkdownItMagicLink, {
 					linksMap: {
 						'vim-jp': { link: 'https://vim-jp.org/', imageUrl: 'https://vim-jp.org/assets/images/vim2-128.png' },
