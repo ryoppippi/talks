df8bf7871dc7b27cb7f5de9f354f771de5f0ec52
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-11-15 17:00:09 +0000

    feat: looooooongcat

---
+++ 2024-11-23/components/LongcatBody.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  class: {
+    type: String,
+    default: ''
+  }
+})
+
+const src = 'https://cdn.jsdelivr.net/gh/mattn/longcat@master/public/themes/longcat/data02.png'
+</script>
+
+<template>
+  <img :src="src" alt="longcat head" :class="props.class" />
+</template>
